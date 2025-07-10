b86268e118b94216246533b4b9f5faf925fb5015
Justicewow <109506750+Hard-working-HN@users.noreply.github.com>
2025-07-10 16:42:14 +0800

    Add files via upload

---
+++ Medium-term/Model/models/STAR.py
@@ -0,0 +1,100 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from models.FECAM import dct_channel_block
+from layers.Embed import DataEmbedding_inverted
+from layers.Transformer_EncDec import Encoder, EncoderLayer
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+class STAR(nn.Module):
+    def __init__(self, d_series, d_core):
+        super(STAR, self).__init__()
+
+        self.gen1 = nn.Linear(d_series, d_series)
+        self.gen2 = nn.Linear(d_series, d_core)
+        self.gen3 = nn.Linear(d_series + d_core, d_series)
+        self.gen4 = nn.Linear(d_series, d_series)
+
+    def forward(self, input, *args, **kwargs):
+        batch_size, channels, d_series = input.shape
+
+        combined_mean = F.gelu(self.gen1(input))
+        combined_mean = self.gen2(combined_mean)
+
+        if self.training:
+            ratio = F.softmax(combined_mean, dim=1)
+            ratio = ratio.permute(0, 2, 1)
+            ratio = ratio.reshape(-1, channels)
+            indices = torch.multinomial(ratio, 1)
+            indices = indices.view(batch_size, -1, 1).permute(0, 2, 1)
+            combined_mean = torch.gather(combined_mean, 1, indices)
+            combined_mean = combined_mean.repeat(1, channels, 1)
+        else:
+            weight = F.softmax(combined_mean, dim=1)
+            combined_mean = torch.sum(combined_mean * weight, dim=1, keepdim=True).repeat(1, channels, 1)
+
+        combined_mean_cat = torch.cat([input, combined_mean], -1)
+        combined_mean_cat = F.gelu(self.gen3(combined_mean_cat))
+        combined_mean_cat = self.gen4(combined_mean_cat)
+        output = combined_mean_cat
+
+        return output, None
+
+
+class Model(nn.Module):
+
+    def __init__(self, configs):
+        super(Model, self).__init__()
+        self.seq_len = configs.seq_len
+        self.pred_len = configs.pred_len
+        self.d_model = configs.d_model
+        self.enc_embedding = DataEmbedding_inverted(configs.seq_len, configs.d_model, configs.dropout)
+        self.use_norm = configs.use_norm
+
+
+        self.encoder = Encoder(
+            [
+                EncoderLayer(
+                    STAR(configs.d_model, configs.d_core),
+                    configs.d_model,
+                    configs.d_ff,
+                    dropout=configs.dropout,
+                    activation=configs.activation,
+                ) for l in range(configs.e_layers)
+            ],
+        )
+
+        self.hist_linear = nn.Linear(self.seq_len, self.d_model).to(device)
+        self.projection = nn.Linear(configs.d_model, configs.pred_len, bias=True)
+
+    def forecast(self, x_enc, x_mark_enc, x_dec, x_mark_dec):
+        if self.use_norm:
+            means = x_enc.mean(1, keepdim=True).detach()
+            x_enc = x_enc - means
+            stdev = torch.sqrt(torch.var(x_enc, dim=1, keepdim=True, unbiased=False) + 1e-5)
+            x_enc /= stdev
+        
+        _, _, N = x_enc.shape
+        dct_model = dct_channel_block(50).to(device)
+
+        climate_vars = x_enc[:, :, :-1].to(device) 
+        target_history = x_enc[:, :, -1:].to(device)  
+
+        climate_vars_dct = dct_model(climate_vars).to(device)
+        enc_input = self.enc_embedding(climate_vars_dct, x_mark_enc)
+        enc_out, attns = self.encoder(enc_input, attn_mask=None) 
+        target_history_new = target_history.permute(0, 2, 1) 
+        target_history_new = self.hist_linear(target_history_new) 
+        enc_out_combined = torch.cat((enc_out, target_history_new), dim=1)
+        dec_out = self.projection(enc_out_combined).permute(0, 2, 1)[:, :, :N]
+
+        # De-Normalization from Non-stationary Transformer
+        if self.use_norm:
+            dec_out = dec_out * (stdev[:, 0, :].unsqueeze(1).repeat(1, self.pred_len, 1))
+            dec_out = dec_out + (means[:, 0, :].unsqueeze(1).repeat(1, self.pred_len, 1))
+        return dec_out
+
+    def forward(self, x_enc, x_mark_enc, x_dec, x_mark_dec, mask=None):
+        dec_out = self.forecast(x_enc, x_mark_enc, x_dec, x_mark_dec)
+        return dec_out[:, -self.pred_len:, :]  
